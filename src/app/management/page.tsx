3ced7da2d243e5c10b5b8a27d6ebdcaca7192214
DARSHAN.P.O <darshanchakure50@gmail.com>
2025-07-12 07:06:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/management/page.tsx
+++ src/app/management/page.tsx
@@ -66,7 +66,7 @@ export default function ManagementPage() {
 
   useEffect(() => {
     fetchDashboardData();
-  }, []);
+  }, [toast]);
 
   const handleAddTeacher = async (e: React.FormEvent) => {
     e.preventDefault();
